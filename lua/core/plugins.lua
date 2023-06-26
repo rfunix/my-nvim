48246b582c74e0a54213b62475d59a312d4ca8d9
rfunix <rafinha.unix@gmail.com>
2023-06-26 10:28:46 -0300

    Add vim-snip

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -52,7 +52,7 @@ return require('packer').startup(function(use)
       "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
       'quangnguyen30192/cmp-nvim-ultisnips', 'hrsh7th/cmp-nvim-lua',
       'octaltree/cmp-look', 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc',
-      'f3fora/cmp-spell', 'hrsh7th/cmp-emoji'
+      'f3fora/cmp-spell', 'hrsh7th/cmp-emoji', 'hrsh7th/vim-vsnip'
     }
   }
 
